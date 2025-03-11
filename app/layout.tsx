f8fb534b9e87769a7bc7c2bcb2852b0e61f1b786
Maicon Furtado <maiconkf@gmail.com>
2025-03-10 23:10:32 -0300

    fix: remove dangerousinnerhtml

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,38 +37,36 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <Head>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "HealthAndBeautyBusiness",
-              name: "Clínica Reabilitare",
-              description:
-                "Clínica especializada em Fisioterapia e Pilates em Pelotas-RS, oferecendo tratamentos personalizados para recuperação e bem-estar.",
-              url: "https://www.clinicareabilitare.com",
-              logo: "https://www.clinicareabilitare.com/logo-reabilitare.png",
-              image: "https://www.clinicareabilitare.com/foto-clinica.jpg",
-              address: {
-                "@type": "PostalAddress",
-                streetAddress: "Rua Dr. Vítor Russomano, 142",
-                addressLocality: "Pelotas",
-                addressRegion: "RS",
-                postalCode: "96077-620",
-                addressCountry: "BR",
-              },
-              contactPoint: {
-                "@type": "ContactPoint",
-                telephone: "+55-53-99166-9801",
-                contactType: "customer service",
-                areaServed: "BR",
-                availableLanguage: "Portuguese",
-              },
-              sameAs: ["https://www.instagram.com/clinicareabilitare"],
-              openingHours: ["Mo-Fr 08:00-20:00"],
-            }),
-          }}
-        />
+        <script type="application/ld+json">
+          {`
+          {
+            "@context": "https://schema.org",
+            "@type": "HealthAndBeautyBusiness",
+            "name": "Clínica Reabilitare",
+            "description": "Clínica especializada em Fisioterapia e Pilates em Pelotas-RS, oferecendo tratamentos personalizados para recuperação e bem-estar.",
+            "url": "https://www.clinicareabilitare.com",
+            "logo": "https://www.clinicareabilitare.com/logo-reabilitare.png",
+            "image": "https://www.clinicareabilitare.com/foto-clinica.jpg",
+            "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "Rua Dr. Vítor Russomano, 142",
+              "addressLocality": "Pelotas",
+              "addressRegion": "RS",
+              "postalCode": "96077-620",
+              "addressCountry": "BR"
+            },
+            "contactPoint": {
+              "@type": "ContactPoint",
+              "telephone": "+55-53-99166-9801",
+              "contactType": "customer service",
+              "areaServed": "BR",
+              "availableLanguage": "Portuguese"
+            },
+            "sameAs": ["https://www.instagram.com/clinicareabilitare"],
+            "openingHours": ["Mo-Fr 08:00-20:00"]
+          }
+        `}
+        </script>
       </Head>
       <body className={`${openSans.className} antialiased`}>{children}</body>
     </html>
