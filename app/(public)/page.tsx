79070491d562cbe29f7640cbcd0556040c162e25
Maicon Furtado <maiconkf@gmail.com>
2025-03-10 23:15:02 -0300

    fix: add json ld in page.tsx

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -8,6 +8,38 @@ import Services from "@/components/services";
 export default function Home() {
   return (
     <div>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "HealthAndBeautyBusiness",
+            name: "Clínica Reabilitare",
+            description:
+              "Clínica especializada em Fisioterapia e Pilates em Pelotas-RS, oferecendo tratamentos personalizados para recuperação e bem-estar.",
+            url: "https://www.clinicareabilitare.com",
+            logo: "https://www.clinicareabilitare.com/logo-reabilitare.png",
+            image: "https://www.clinicareabilitare.com/foto-clinica.jpg",
+            address: {
+              "@type": "PostalAddress",
+              streetAddress: "Rua Dr. Vítor Russomano, 142",
+              addressLocality: "Pelotas",
+              addressRegion: "RS",
+              postalCode: "96077-620",
+              addressCountry: "BR",
+            },
+            contactPoint: {
+              "@type": "ContactPoint",
+              telephone: "+55-53-99166-9801",
+              contactType: "customer service",
+              areaServed: "BR",
+              availableLanguage: "Portuguese",
+            },
+            sameAs: ["https://www.instagram.com/clinicareabilitare"],
+            openingHours: ["Mo-Fr 08:00-20:00"],
+          }),
+        }}
+      />
       <div className="bg-(--color-emerald-400) rounded-b-[32px]">
         <Header />
         <Main />
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const openSans = Open_Sans({
   subsets: ["latin"],
@@ -36,38 +35,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <Head>
-        <script type="application/ld+json">
-          {`
-          {
-            "@context": "https://schema.org",
-            "@type": "HealthAndBeautyBusiness",
-            "name": "Clínica Reabilitare",
-            "description": "Clínica especializada em Fisioterapia e Pilates em Pelotas-RS, oferecendo tratamentos personalizados para recuperação e bem-estar.",
-            "url": "https://www.clinicareabilitare.com",
-            "logo": "https://www.clinicareabilitare.com/logo-reabilitare.png",
-            "image": "https://www.clinicareabilitare.com/foto-clinica.jpg",
-            "address": {
-              "@type": "PostalAddress",
-              "streetAddress": "Rua Dr. Vítor Russomano, 142",
-              "addressLocality": "Pelotas",
-              "addressRegion": "RS",
-              "postalCode": "96077-620",
-              "addressCountry": "BR"
-            },
-            "contactPoint": {
-              "@type": "ContactPoint",
-              "telephone": "+55-53-99166-9801",
-              "contactType": "customer service",
-              "areaServed": "BR",
-              "availableLanguage": "Portuguese"
-            },
-            "sameAs": ["https://www.instagram.com/clinicareabilitare"],
-            "openingHours": ["Mo-Fr 08:00-20:00"]
-          }
-        `}
-        </script>
-      </Head>
       <body className={`${openSans.className} antialiased`}>{children}</body>
     </html>
   );
